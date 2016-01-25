67db39a6d9a34816188a6f19808412b246fec4a1
0scaD <oscardillon@rocketmail.com>
2016-01-25 12:45:18 -0600

    changes 1-9

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section02methods/TriangleShell.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section02methods/TriangleShell.java
@@ -1,5 +1,7 @@
 package org.teachingkidsprogramming.section02methods;
 
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 @SuppressWarnings("unused")
 public class TriangleShell
@@ -8,20 +10,37 @@ public class TriangleShell
   public static void main(String[] args)
   {
     // Show the tortoise --#1
+    Tortoise.show();
     // Make the tortoise go as fast as possible --#6
+    Tortoise.setSpeed(10);
     // Do the following 60 times --#7.1
-    //      Change the pen color of the line the tortoise draws to a random color --#9
-    //      Increase the current length of the side by 4 pixels --#8
-    //      drawTriangle (recipe below) --#5.1
-    //
+    for (int i = 0; i < 60; i++)
+    {
+      //      Change the pen color of the line the tortoise draws to a random color --#9
+      Tortoise.setPenColor(PenColors.getRandomColor());
+      //      Increase the current length of the side by 4 pixels --#8
+      Tortoise.setPenWidth(4);
+      //      drawTriangle (recipe below) --#5.1
+      //
+      drawTriangle();
+      //
+      //      Turn the tortoise 1/60th of 360 degrees to the right --#10
+      // Repeat --#7.2
+    }
+  }
+  private static void drawTriangle()
+  {
+    int currentLength = 90;
     //      ------------- Recipe for drawTriangle --#5.2
     //          Do the following 3 times --#3.1
-    //              Move the tortoise using the current length --#4
-    //              Turn the tortoise 1/3rd of 360 degrees --#2
-    //          Repeat --#3.2
+    for (int i = 0; i < 3; i++)
+    {
+      //              Move the tortoise using the current length --#4
+      Tortoise.move(currentLength);
+      //              Turn the tortoise 1/3rd of 360 degrees --#2
+      Tortoise.turn(360 / 3);
+      //          Repeat --#3.2
+    }
     //      ------------- End of drawTriangle recipe --#5.3
-    //
-    //      Turn the tortoise 1/60th of 360 degrees to the right --#10
-    // Repeat --#7.2
   }
 }
