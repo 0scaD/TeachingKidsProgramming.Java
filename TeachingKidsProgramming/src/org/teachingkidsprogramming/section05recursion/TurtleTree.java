9e142c24cab6422d30e2ba4afc21cd4aeaec92cd
0scaD <oscardillon@rocketmail.com>
2016-02-10 13:00:28 -0600

    redo's

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section05recursion/TurtleTree.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section05recursion/TurtleTree.java
@@ -25,33 +25,37 @@ public class TurtleTree
     //    ------------- Recipe for drawBranch --#2.2
     //        If the current branch length is greater than zero, do the rest of this recipe --#5
     if (length > 0)
+      ;
     {
-    //        adjustColor (recipe below)--#15.1
-    //        ------------- Recipe for adjustColor --#15.2
-    HashMap<Integer, Color> colors = new HashMap<Integer, Color>();
-    //            A 10 pixel long branch is lime --#20
-    //            A 20 pixel long branch is forest green --#19
-    //            A 30 pixel long branch is dark green --#18
-    //            A 40 pixel long branch is olive --#17
-    //            A 50 pixel long branch is sienna --#14
-    //            A 60 pixel long branch is saddle brown (TIP: Put the values into the 'colors' HashMap)--#13 
-    //            Get the value of the branch length from the 'colors' HashMap and use that to set the pen color --#21
-    //        ------------- End of adjustColor --#15.3
-    //
-    //        Move the tortoise the length of the current branch --#1.1
-    Tortoise.move(length);
-    //        draw LowerBranches (recipe below) --#6.1
-    //
-    drawLowerBranches();
-    //
+      //        adjustColor (recipe below)--#15.1
+      //        ------------- Recipe for adjustColor --#15.2
+      HashMap<Integer, Color> colors = new HashMap<Integer, Color>();
+      //            A 10 pixel long branch is lime --#20
+      //            A 20 pixel long branch is forest green --#19
+      //            A 30 pixel long branch is dark green --#18
+      //            A 40 pixel long branch is olive --#17
+      //            A 50 pixel long branch is sienna --#14
+      //            A 60 pixel long branch is saddle brown (TIP: Put the values into the 'colors' HashMap)--#13 
+      //            Get the value of the branch length from the 'colors' HashMap and use that to set the pen color --#21
+      //        ------------- End of adjustColor --#15.3
+      //
+      //        Move the tortoise the length of the current branch --#1.1
+      Tortoise.move(length);
+      //        draw LowerBranches (recipe below) --#6.1
+      //
+      drawLowerBranches();
+      //
     }
     //    ------------- End of drawBranch recipe --#2.3
   }
   private static void drawLowerBranches()
   {
     //        ------------- Recipe for drawLowerBranches --#6.2
+    int branchLength = 60;
     //            Turn the Tortoise 30 degrees to the right --#3
     Tortoise.turn(30);
+    //            drawShorterBranch (recipe below) --#8.1
+    //
     drawShorterBranch();
     //
     //            Turn the Tortoise 60 degrees to the left --#7
@@ -59,14 +63,14 @@ public class TurtleTree
     //            drawShorterBranch --#9
     drawShorterBranch();
     //            Turn the Tortoise 30 degrees to the right --#12
+    Tortoise.turn(30);
     //            adjustColor --#16
     //            Move the tortoise backward the length of the current branch --#11
+    Tortoise.move(-branchLength);
     //        ------------- End of drawLowerBranches recipe --#6.3
   }
   private static void drawShorterBranch()
   {
-    //            drawShorterBranch (recipe below) --#8.1
-    //
     //            ------------- Recipe for drawShorterBranch --#8.2
     //                drawBranch (10 pixels shorter) --#4
     int drawBranch = 50;
